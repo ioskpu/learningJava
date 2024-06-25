709c285411a847dc646424d6476dc48041a19692
ioskpu <lrcorales@gmail.com>
2024-06-25 16:23:40 -0500

    se agrega proyecto de operadores

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,25 @@
+
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Luis
+ */
+public class OperadoresAritmeticos {
+
+    
+    public static void main(String[] args) {
+        // se declara las variables
+        int num1 = 7;
+        int num2 = 4;
+        
+        //se realizan las operaciones aritmeticas por consola
+        System.out.println("la suma de las variables es: " + (num1 + num2));
+        System.out.println("la resta de las variables es: " + (num1 - num2));
+        System.out.println("la multiplicacion de las variables es: " + (num1 * num2));
+        System.out.println("la división de las variables es: " + (num1 / num2));
+        System.out.println("el modulo de las variables es: " + (num1 % num2));
+        
+    }
+    
+}
