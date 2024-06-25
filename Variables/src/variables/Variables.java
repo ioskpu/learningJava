bd86b90d52d2870fbbea455e08cdffa58a9fa31e
ioskpu <lrcorales@gmail.com>
2024-06-24 19:15:37 -0500

    se agrega proyecto de variables

---
+++ Variables/src/variables/Variables.java
@@ -0,0 +1,23 @@
+
+package variables;
+
+/**
+ *
+ * @author Luis
+ */
+public class Variables {
+
+   
+    public static void main(String[] args) {
+        int telefono = 6245656;
+        char letra = 'G';
+        float peso = 32.2f;
+        double ngrande = 70.122345679;
+        
+        //imprimir consola
+        System.out.println("EL numero de telefono es:" + telefono);
+        System.out.println("la letra es: " + letra + ", el peso: " + peso);
+        System.out.println("el numero grande es: " + ngrande);
+    }
+    
+}
