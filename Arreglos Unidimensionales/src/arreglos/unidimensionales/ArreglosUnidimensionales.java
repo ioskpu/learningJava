7d047692fef5497ddf92e2b7700da117505c7f2f
ioskpu <lrcorales@gmail.com>
2024-07-25 09:12:45 -0500

    se agrega proyecto arreglos

---
+++ Arreglos Unidimensionales/src/arreglos/unidimensionales/ArreglosUnidimensionales.java
@@ -0,0 +1,16 @@
+
+package arreglos.unidimensionales;
+
+
+public class ArreglosUnidimensionales {
+
+    
+    public static void main(String[] args) {
+        int notas[] = {2,4,6,8,10,12};
+        
+        for(int i = 0; i < notas.length; i++){
+            System.out.println("el valor de notas es: " + notas[i]);
+        };
+    }
+    
+}
