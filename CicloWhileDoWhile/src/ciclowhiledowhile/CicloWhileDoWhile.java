f10e30cf5bf1c72cd30f0c0e0df77cb0a8b2acc0
ioskpu <lrcorales@gmail.com>
2024-07-25 08:57:26 -0500

    se agrega proyecto ciclo while y do while

---
+++ CicloWhileDoWhile/src/ciclowhiledowhile/CicloWhileDoWhile.java
@@ -0,0 +1,25 @@
+
+package ciclowhiledowhile;
+
+
+public class CicloWhileDoWhile {
+
+    
+    public static void main(String[] args) {
+       /* //ciclo while
+        int x = 1;
+        
+        while(x <= 10){
+            System.out.println("El valos de x es = " + x);
+            x++;
+        }*/
+       
+       //ciclo do while
+       int x = 0;
+       
+       do{
+           System.out.println("el valor de i es = " + x);
+           x++;
+       }while(x <=10);
+    }    
+}
