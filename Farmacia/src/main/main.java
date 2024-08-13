85f3b603c66ca62c1c3f89cde3e975b1fa7afa8d
ioskpu <lrcorales@gmail.com>
2024-08-13 11:18:19 -0500

    se agrega proyecto Farmacia, proyecto del curso, en este caso Login

---
+++ Farmacia/src/main/main.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import views.LoginView;
+
+
+public class main {
+    public static void main(String[] args) {
+        //Instanciar login
+        LoginView login = new LoginView();
+        login.setVisible(true);
+    }
+    
+}
